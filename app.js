10ef0e46c6368c422e42747568f4ef077268ebb2
Corban Brook <corbanbrook@gmail.com>
2012-03-14 10:34:54 -0400

    Breaking out routes into separate files.

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-var routes = require('./routes');
 var settings = require('./settings');
 
 var app = settings.app;
 
-
-app.get('/', routes.index);
-app.get('/ping', routes.ping);
-app.post('/login', routes.login);
-app.post('/message', routes.message);
-app.get('/logout', routes.logout);
+// routes
+require("./routes")(app);
+require("./routes/message")(app);
+require("./routes/auth")(app);
 
 app.listen(settings.options.port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
