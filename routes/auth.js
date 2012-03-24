b423d9c5dbb867c18bf7bff11c6ae640b2c27999
Jen Fong-Adwent <jen@ednapiranha.com>
2012-03-24 09:10:49 -0400

    using client-sessions instead

--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,6 @@ module.exports = function(app, settings, io, userList) {
   app.post("/login", function(req, res) {
     auth.verify(req, settings, function(error, email) {
       if(email) {
-        res.cookie('rememberme', 'yes', {
-          secure: settings.options.secureCookie,
-          httpOnly: true
-        });
         req.session.email = email;
         req.session.userFont = Math.floor(Math.random() * 8);
         req.session.nickname = 'i_love_ie6';
