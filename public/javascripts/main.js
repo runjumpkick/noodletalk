65e861dde0077ccecb842b809737daed3e72d4fe
John C. Worsley <lx@openvein.com>
2012-03-25 16:36:33 -0700

    Added lightboxy hush/unhush method in jquery to enable the version checker to do some snazzy visuals. Also added said version checker. Also added a common "noodle" referent to each route, with access to package.json (mostly for noodle.version for now but may be useful in future).

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -11,6 +11,8 @@ $(function() {
   var mediaVideoMatcher = /<video\s.+>.+<\/video>/i;
   var mediaAudioMatcher = /<audio\s.+>.+<\/audio>/i;
   var isSubmitting = false;
+  var localVersion = undefined;
+  var hushLock = 0;
 
   var updateMedia = function(data) {
     // Update the media
@@ -26,7 +28,43 @@ $(function() {
       }
     }
   };
-
+  
+  var hush = function(content,contentID,timeToFadeIn,timeToAppear) {
+    if (!hushLock)
+    {
+      hushLock = 1;
+      
+      // Disable messaging.
+      $('input[name=message]').attr('disabled','disabled');
+      
+      // Disable scrolling.
+      disableScroll();
+      
+      setTimeout(function() {
+        setTimeout(function()
+        {
+          var newElement = jQuery(content);
+          newElement.attr('id', contentID);
+          newElement.attr('class', 'hush');
+          $('body').append(newElement);
+          $('#'+contentID).animate({'width':440,'height':338, 'margin-left': -220, 'margin-top': -184}, timeToAppear, function() {});
+        },timeToAppear);
+        $('#hush').fadeIn();
+      },timeToFadeIn);
+    }
+  }
+  
+  var unHush = function(contentID,timeToFadeOut,timeToDisappear) {
+    setTimeout(function() {
+      setTimeout(function()
+      {
+        $('#hush').fadeOut();
+        hushLock = 0;
+      },timeToFadeOut);
+      $('#'+contentID).animate({'width':0,'height':0, 'margin-left': 0, 'margin-top': 0}, timeToDisappear, function() {});
+    },timeToDisappear);
+  }
+  
   var updateMessage = function(data) {
     // Update the message
     var message = $.trim(data.message);
@@ -77,6 +115,14 @@ $(function() {
     
     messagesUnread += 1;
     document.title = 'Noodle Talk (' + messagesUnread + ')';
+    
+    // Version checking: if we have a mismatch of our local version and the server version force a refresh.
+    if (localVersion === undefined)
+    {
+      localVersion = data.version;
+    } else if (localVersion != data.version) {
+      hush('<img onclick="window.location.reload()" src="/images/please_refresh.gif" />', 'refresh', 500, 1000);
+    }
   };
 
   // if the user just landed on this page, get the recent messages
